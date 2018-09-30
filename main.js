a832dc2d1132872bf7627ac1e33fd147a9a3bc6a
interest1 <927332199@qq.com>
2018-09-30 14:04:37 +0800

    first time

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// document.body.style.backgroundColor="lightblue";
+
+document.querySelector('#btn-readmore').click()
+document.querySelector('body > div.pulllog-box > div > div > button.btn-close').click()
+document.querySelector('#wrapper > div.logo-container').click()
