c00bd9133692a277856dcd8fca77ba4c5caa920f
interest1 <927332199@qq.com>
2018-09-30 15:01:46 +0800

    remove ads

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-
-// document.body.style.backgroundColor="lightblue";
-
+//阅读更多的模拟点击
 document.querySelector('#btn-readmore').click()
+//登录叉号的模拟点击
 document.querySelector('body > div.pulllog-box > div > div > button.btn-close').click()
-document.querySelector('#wrapper > div.logo-container').click()
+
+//广告的隐藏
+var ads1=document.querySelector('#mainBox > aside > div.csdn-tracking-statistics.mb8.box-shadow');
+var ads2=document.querySelector('#asideFooter > div:nth-child(1)');
+
+ads1.style.display='none'
+ads2.style.display='none'
