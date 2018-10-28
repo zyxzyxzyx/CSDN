b39de21e7955e6eac9131f3b36268d6c027a1669
interest1 <927332199@qq.com>
2018-10-28 22:35:32 +0800

    add comments

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ if(exist('#btn-readmore')){
 document.querySelector('body > div.pulllog-box > div > div > button.btn-close').click()
 
 //广告的隐藏
-// var ads1=document.querySelector('#mainBox > aside > div.csdn-tracking-statistics.mb8.box-shadow');
-// var ads2=document.querySelector('#asideFooter > div:nth-child(1)');
-var ads2=document.querySelector('#mainBox > aside');
+var ads1=document.querySelector('#mainBox > aside > div.csdn-tracking-statistics.mb8.box-shadow');
+var ads2=document.querySelector('#asideFooter > div:nth-child(1)');
+// var ads2=document.querySelector('#mainBox > aside');
 
-// ads1.style.display="none";
+ads1.style.display="none";
 ads2.style.display="none";
 
 document.body.onclick=moveRight;
